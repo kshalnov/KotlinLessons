b97207dc1c7178196555a25dbb50eea648158076
Kirill Shalnov <borhammere@gmail.com>
2021-10-26 20:31:00 +0300

    add Java and Kotlin activities

---
+++ app/src/main/java/ru/gb/course1/myapplication/KotlinActivity.java
@@ -0,0 +1,50 @@
+package ru.gb.course1.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KotlinActivity extends AppCompatActivity {
+    private Button plusButton;
+    private Button minusButton;
+    private TextView counterTextView;
+
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+
+        initViews();
+
+        counterTextView.setTextColor(Color.BLUE);
+
+        updateCounterTextView(counter);
+
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateCounterTextView(--counter);
+            }
+        });
+
+        plusButton.setOnClickListener(view -> {
+            updateCounterTextView(++counter);
+        });
+    }
+
+    private void updateCounterTextView(int counter) {
+        counterTextView.setText(String.valueOf(counter));
+    }
+
+    private void initViews() {
+        minusButton = findViewById(R.id.minus_button);
+        plusButton = findViewById(R.id.plus_button);
+        counterTextView = findViewById(R.id.counter_text_view);
+    }
+}
--- app/src/main/java/ru/gb/course1/myapplication/MainActivity.java
+++ app/src/main/java/ru/gb/course1/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package ru.gb.course1.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button javaButton;
+    private Button kotlinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        javaButton = findViewById(R.id.java_button);
+        javaButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, JavaActivity.class);
+            startActivity(intent);
+        });
+
+        kotlinButton = findViewById(R.id.kotlin_button);
+        kotlinButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, KotlinActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
