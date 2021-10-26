ee3c3e886d032041e5cf6e42d44f629d95d8307d
Kirill Shalnov <borhammere@gmail.com>
2021-10-26 21:35:59 +0300

    convert KotlinActivity to Kotlin

--- app/src/main/java/ru/gb/course1/myapplication/KotlinActivity.kt
+++ app/src/main/java/ru/gb/course1/myapplication/KotlinActivity.kt
@@ -1,50 +1,50 @@
-package ru.gb.course1.myapplication;
+package ru.gb.course1.myapplication
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class KotlinActivity : AppCompatActivity() {
+    private lateinit var plusButton: Button
+    private lateinit var minusButton: Button
+    private lateinit var counterTextView: TextView
 
-public class KotlinActivity extends AppCompatActivity {
-    private Button plusButton;
-    private Button minusButton;
-    private TextView counterTextView;
+    private var counter = 0
 
-    private int counter = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter)
+        initViews()
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_counter);
+        counterTextView.setTextColor(Color.BLUE)
 
-        initViews();
+        updateCounterTextView(counter)
 
-        counterTextView.setTextColor(Color.BLUE);
-
-        updateCounterTextView(counter);
-
-        minusButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                updateCounterTextView(--counter);
+//        minusButton.setOnClickListener { updateCounterTextView(--counter) }
+        minusButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                updateCounterTextView(--counter)
             }
-        });
+        })
+//        plusButton.setOnClickListener({ v: View ->
+//            updateCounterTextView(++counter)
+//        })
+        plusButton.setOnClickListener() {
+            updateCounterTextView(++counter)
+        }
 
-        plusButton.setOnClickListener(view -> {
-            updateCounterTextView(++counter);
-        });
     }
 
-    private void updateCounterTextView(int counter) {
-        counterTextView.setText(String.valueOf(counter));
+    private fun updateCounterTextView(counter: Int) {
+        counterTextView.text = counter.toString()
     }
 
-    private void initViews() {
-        minusButton = findViewById(R.id.minus_button);
-        plusButton = findViewById(R.id.plus_button);
-        counterTextView = findViewById(R.id.counter_text_view);
+    private fun initViews() {
+        minusButton = findViewById(R.id.minus_button)
+        plusButton = findViewById(R.id.plus_button)
+        counterTextView = findViewById(R.id.counter_text_view)
     }
 }
